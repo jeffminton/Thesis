741be6926321c464807e2db330ff148764c6fd59
Jeffrey Minton <jeffrey.minton@gmail.com>
2011-01-23 23:09:59 -0500

    working on getting c++ to call matlab

---
+++ diabot/diabot.cpp
@@ -0,0 +1,29 @@
+//#include "stdafx.h"
+#include "Cxml.h"
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef _CHAR16T
+#define CHAR16_T
+#endif
+#include <engine.h>
+
+
+int main()
+{
+	Engine *ep;
+	
+	/*
+	 * Start the MATLAB engine locally by executing the string
+	 * "matlab"
+	 *
+	 * To start the session on a remote host, use the name of
+	 * the host as the string rather than \0
+	 *
+	 * For more complicated cases, use any string with whitespace,
+	 * and that string will be executed literally to start MATLAB
+	 */
+	if (!(ep = engOpen("\0"))) {
+		fprintf(stderr, "\nCan't start MATLAB engine\n");
+		return -1;
+	}
+}
