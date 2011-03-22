efa058bd93afe4b081beb5a4de1ee682b9f72c13
Jeffrey Minton <jeffrey.minton@gmail.com>
2011-03-22 12:16:38 -0400

    created 5 by 5 pixel example for python, joined drafts into one full draft

--- Research/blobing/blob.py
+++ Research/blobing/blob.py
@@ -20,6 +20,12 @@
 #	   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #	   MA 02110-1301, USA.
 
+img5x5 = [[[120, 111, 82],[118, 109, 80],[117, 108, 79],[50, 70, 79],[51, 71, 81]],
+[[121, 112, 83],[120, 111, 82],[51, 70, 80],[50, 72, 81],[49, 69, 82]],
+[[121, 112, 83],[50, 111, 201],[50, 109, 200],[51, 115, 204],[67, 200, 100]],
+[[200, 50, 90],[225, 104, 67],[50, 115, 202],[73, 112, 201],[72, 100, 203]],
+[[201, 70, 100],[100, 234, 80],[103, 231, 79],[99, 229, 78],[98, 228, 77]]]
+
 import cv
 import time
 from unionfind import *
@@ -39,7 +45,28 @@ class blobit():
 		#~ cv.SetCaptureProperty(self.cap, cv.CV_CAP_PROP_FRAME_WIDTH, self.width)
 		#~ cv.SetCaptureProperty(self.cap, cv.CV_CAP_PROP_FRAME_HEIGHT, self.height)
 
-
+	def gen5x5(self):
+		r = img5x5[:][:][2]
+		g = img5x5[:][:][1]
+		b = img5x5[:][:][0]
+		
+		for y in range(5):
+			for x in range(5):
+				b = img5x5[x][y][2]
+				img5x5[x][y][2] = img5x5[x][y][0]
+				img5x5[x][y][0] = b
+					
+		imgIn = cv.LoadImage("segOrig.jpg")
+		imgIn = imgIn[200:205, 200:205]
+		
+		for y in range(imgIn.height):
+			for x in range(imgIn.width):
+				#~ print "y: ", y
+				#~ print "x: ", x
+				#~ print(img5x5[x][y])
+				imgIn[x, y] = img5x5[x][y]
+		return imgIn
+	
 	def whereObject(self):
 		orig = cv.QueryFrame(self.cap)
 		orig = cv.QueryFrame(self.cap)
